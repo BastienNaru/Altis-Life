f601267d50076083867ad4ac8d4d497771e45a51
Anis <crylegend95@gmail.com>
2015-02-23 22:00:13 +0100

    Centrage téléphone (sort des écrans de certains)

--- Altis_Life.Altis/dialog/cell_phone.hpp
+++ Altis_Life.Altis/dialog/cell_phone.hpp
@@ -12,8 +12,8 @@ class Life_cell_phone {
 		{
 			idc = 3001;
 			text = "textures\autre\telephone.paa";
-			x = 1.1675;
-			y = 0.3;
+			x = 0.5675;
+			y = -0.00433818;
 			w = 0.4;
 			h = 1;
 		};
@@ -28,8 +28,8 @@ class Life_cell_phone {
 			colorBackground[] = {0, 0, 0, 0};
 			idc = 3002;
 			text = "$STR_CELL_TextToSend";
-			x = 1.2125;
-			y = 0.510809;
+			x = 0.605607;
+			y = 0.210809;
 			w = 0.2725;
 			h = 0.04;
 		};
@@ -40,7 +40,7 @@ class Life_cell_phone {
 		idc = 3003;
 		
 		text = "";
-		x = 1.2125; y = 0.640086;
+		x = 0.607904; y = 0.340086;
 		w = 0.325; h = 0.26;
 		
 		};
@@ -53,8 +53,8 @@ class Life_cell_phone {
 			onButtonClick = "[] call TON_fnc_cell_textmsg";
 			colorActive[] = {0,0,0,0};
 			
-			x = 1.2125;
-			y = 0.94;
+			x = 0.6125;
+			y = 0.64;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -63,7 +63,7 @@ class Life_cell_phone {
 		{
 			idc = 3004;
 			
-			x = 1.2125; y = 0.58;
+			x = 0.6125; y = 0.28;
 			w = 0.2375; h = 0.04;
 		};
 
@@ -74,8 +74,8 @@ class Life_cell_phone {
 			colorBackground[] = {0,0.23,1,0.48};
 			onButtonClick = "[] call TON_fnc_cell_textcop";
 			
-			x = 1.375;
-			y = 0.94;
+			x = 0.775;
+			y = 0.64;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -83,12 +83,12 @@ class Life_cell_phone {
 		class TextAdminButton : life_RscButtonMenu 
 		{
 			idc = 3017;
-			text = "Aide";
+			text = "Besoin d'aide";
 			colorBackground[] = {0,0.23,1,0.48};
 			onButtonClick = "[] call TON_fnc_cell_textadmin";
 			
-			x = 1.375;
-			y = 1.02;
+			x = 0.775;
+			y = 0.72;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -100,8 +100,8 @@ class Life_cell_phone {
 			colorBackground[] = {0,0.23,1,0.48};
 			onButtonClick = "[] call TON_fnc_cell_adminmsg";
 			
-			x = 1.2125;
-			y = 1.1;
+			x = 0.6125;
+			y = 0.8;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -113,8 +113,8 @@ class Life_cell_phone {
 			colorBackground[] = {0,0.23,1,0.48};
 			onButtonClick = "[] call TON_fnc_cell_adminmsgall";
 			
-			x = 1.375;
-			y = 1.1;
+			x = 0.775;
+			y = 0.8;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -126,8 +126,8 @@ class Life_cell_phone {
 			colorBackground[] = {0,0.23,1,0.48};
 			onButtonClick = "[] call TON_fnc_cell_emsrequest";
 			
-			x = 1.2125;
-			y = 1.02;
+			x = 0.6125;
+			y = 0.72;
 			w = 0.15;
 			h = 0.05;
 		};
@@ -137,9 +137,9 @@ class Life_cell_phone {
 			text = "";
 			onButtonClick = "closeDialog 0;";
 			colorBackground[] = {0,0,0,0};
-			x = 1.36;
-			y = 1.19;
-			w = 0.065;
+			x = 0.75;
+			y = 0.89;
+			w = 0.05;
 			h = 0.06;
 		};
 	};
